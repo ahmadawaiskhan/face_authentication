b8a597a4af8cf90d18d57afe3a79f113bbbbf79b
Ahmad Awais <111912402+ahmadawaiskhan@users.noreply.github.com>
2024-08-15 13:57:52 +0500

    Update database_constants.py

--- face_auth/constant/database_constants.py
+++ face_auth/constant/database_constants.py
@@ -1,5 +1,5 @@
 # Hardcoded MongoDB details
 MONGODB_URL_KEY = "mongodb+srv://awais:awais1122@cluster0.crj8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "UserDatabase"
-USER_COLLECTION_NAME = "Users"
+USER_COLLECTION_NAME = "User"
 EMBEDDING_COLLECTION_NAME = "Embeddings"
